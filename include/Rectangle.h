69890d4b919d9f17091c256297d0220cb4f3c259
aaronhehehaha <tungkin_h@hotmail.com>
2015-11-04 12:35:21 -0800

    Add rectangle class

---
+++ include/Rectangle.h
@@ -0,0 +1,19 @@
+class Rectangle{
+
+    public:
+        Rectangle(float t, float b, float l, float r);
+        float getTop();
+        float getBottom();
+        float getLeft();
+        float getRight();
+
+    private:
+        float top;
+        float bottom;
+        float left;
+        float right;
+
+};
+
+bool Intersect(Rectangle rect1, Rectangle rect2);
+bool Contains(Rectangle rect, float x, float y);
